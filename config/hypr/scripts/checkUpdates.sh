0e68b84cfa95dfd3ecf698debde38d39ae847da3
Brian C. Arnold <brian.arnold@spiderrock.net>
2023-07-10 16:02:21 -0400

    Updated how checkUpdates works.

--- config/hypr/scripts/checkUpdates.sh
+++ config/hypr/scripts/checkUpdates.sh
@@ -1,32 +1,51 @@
 #!/usr/bin/env bash
 
   
-git -C ~/dotfiles fetch
 
-touch ~/.cache/dotFilesUpdateChecked.touch
-
-git -C ~/dotfiles merge-base --is-ancestor origin/main HEAD
-
-if [ $? -eq 0 ]; then
-  notify-send -a '~/dotfiles' -i vcs-normal "Up to date."
-else
-  notify-send -a '~/dotfiles' -i vcs-update-required "Update available."
-fi
+#if [ "$isNormal" = "false" ]; then
+#  notify-send -a '~/dotfiles' -i vcs-update-required "$message"
+#else
+#fi
 
 update_daemon() {
-	while :; do 
-  
+    while :; do 
+
     git -C ~/dotfiles fetch
-    
+
     touch ~/.cache/dotFilesUpdateChecked.touch
-    
+
+    message=""
+
+    hasChanges=$(git status --untracked-files=no -s | wc -l)
+
+    isNormal="true"
+
+    icon="vcs-normal"
+
     git -C ~/dotfiles merge-base --is-ancestor origin/main HEAD
 
     if [ $? -eq 0 ]; then
-      :
+      message="Up to date"
     else
-      notify-send -a '~/dotfiles' -i vcs-update-required "Update available."
+      message="Update available"
+      icon="vcs-update-required"
+      #notify-send -a '~/dotfiles' -i vcs-update-required "Update available."
     fi
+
+    if [ $hasChanges -ne 0 ]; then
+      isNormal="false"
+      if [ "$icon" != "vcs-normal" ]; then
+        icon="vcs-conflicting"  
+      else
+        icon="vcs-locally-modified"
+      fi;
+      message="$message, but changes detected."
+    else
+      message="$message."
+    fi
+
+    notify-send -a '~/dotfiles' -i "$icon" "$message"
+  
     sleep 3600;
     
   done;
