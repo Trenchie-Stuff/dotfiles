96f7e4bb087502d8f4286e2463bce3ec3b375d4b
Brian C. Arnold <brian.arnold@spiderrock.net>
2023-07-10 15:28:14 -0400

    Updates to fix issues with Discord REFUSING to forward notifications if I'm logged in.

--- config/hypr/scripts/lock.sh
+++ config/hypr/scripts/lock.sh
@@ -4,5 +4,12 @@ lowerName=`echo $name | tr '[:upper:]' '[:lower:]'`
 if [[ $lowerName == *"steam"* ]]; then
     :
 else
+    monitors=$(hyprctl monitors)
+    activemonitor=$(echo $monitors | grep -B 7 "focused: yes" | awk 'NR==1 {print $2}')
+    activews=$(echo $monitors | grep -A 2 "$activemonitor" | awk 'NR==3 {print $1}' RS='(' FS=')')
+    hyprctl dispatch workspace 9
+    killall -9 Discord
     swaylock
+    discord&
+    hyprctl dispatch workspace activews
 fi
